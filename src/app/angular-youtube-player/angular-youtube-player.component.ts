f13ac826e77be5eee777f23a7c8cf87fe3030da6
Taillefer Jordan <jtaillefer@excilys.com>
2018-07-31 14:10:17 +0200

    (feat) add youtube player api

--- src/app/angular-youtube-player/angular-youtube-player.component.ts
+++ src/app/angular-youtube-player/angular-youtube-player.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-angular-youtube-player',
   templateUrl: './angular-youtube-player.component.html',
   styleUrls: ['./angular-youtube-player.component.scss']
 })
-export class AngularYoutubePlayerComponent implements OnInit {
+export class AngularYoutubePlayerComponent implements OnInit, OnChanges {
+  public YT: any;
+  @Input() public videoId: any;
+  @Input() public height = '360';
+  @Input() public width = '640';
+  public player: any;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  init() {
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
   }
 
+  ngOnInit() {
+    this.init();
+
+    window['onYouTubeIframeAPIReady'] = e => {
+      this.YT = window['YT'];
+      this.player = new window['YT'].Player('player', {
+        videoId: this.videoId,
+        height: this.height,
+        width: this.width,
+        events: {
+          onStateChange: this.onPlayerStateChange.bind(this),
+          onError: this.onPlayerError.bind(this)
+        }
+      });
+    };
+  }
+
+  onPlayerStateChange(event) {
+    console.log(event);
+    switch (event.data) {
+      case window['YT'].PlayerState.PLAYING:
+        if (this.cleanTime() === 0) {
+          console.log('started ' + this.cleanTime());
+        } else {
+          console.log('playing ' + this.cleanTime());
+        }
+        break;
+      case window['YT'].PlayerState.PAUSED:
+        if (this.player.getDuration() - this.player.getCurrentTime() !== 0) {
+          console.log('paused' + ' @ ' + this.cleanTime());
+        }
+        break;
+      case window['YT'].PlayerState.ENDED:
+        console.log('ended ');
+        break;
+    }
+  }
+  cleanTime() {
+    return Math.round(this.player.getCurrentTime());
+  }
+  onPlayerError(event) {
+    switch (event.data) {
+      case 2:
+        console.log('' + this.videoId);
+        break;
+      case 100:
+        break;
+      case 101 || 150:
+        break;
+    }
+  }
+
+  ngOnChanges(): void {
+    this.player.loadVideoById(this.videoId);
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,13 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-
-  `
+  templateUrl: 'app.component.html'
 })
 export class AppComponent {
   @Input() title = 'angular-youtube-player';
+  videoId = 'FOeRjzYH9JA';
+
+  click() {
+    this.videoId = 'u1RSQIVHTLg';
+  }
 }
